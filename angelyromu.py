7d5fe858a4273df5d674c7b09a2dd7a913c3fd93
romu26 <diaz.pimentel.romualdo@gmail.com>
2024-09-30 10:06:27 +0200

    Add files via upload

---
+++ angelyromu.py
@@ -0,0 +1,13 @@
+#esto es un programa que suma dos numeros
+print("Dime un número:")
+Num1= int(input())
+
+print("dime otro número")
+num2 =int(input())
+
+
+#realizamos la operacion de suma
+
+total=Num1 + num2
+
+print("el resultado de sumar", Num1,"+", num2,"=",total)
